38bfdb27e41e4815910e08a8e746af58c63680d0
Soggard <e.bernet@laposte.net>
2019-03-20 15:30:10 +0100

    Affiche des liens statiques

--- hello-world/Home.js
+++ hello-world/Home.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
 
 
 export default class App extends React.Component {
@@ -11,35 +12,49 @@ export default class App extends React.Component {
 
     render() {
         const text = this.state.clicked ? 'cliqué' : 'Pas cliqué';
+        try {
+            //const result = await this.getGames()
+        } catch(exeption) {
+            //const result = []
+        }
+
+
         return (
             <View style={styles.container}>
-                <Text style={styles.text}>{text}</Text>
-                <Text style={styles.button1} onPress={() => {
-                    // Mon code réagissant à l'event
-                    this.setState({clicked: false})
-                }}
-                >Pas cliqué</Text>
+                <Text style={styles.text}>Hello Games</Text>
 
-                <Text style={styles.button2}
-                      onPress={() =>
-                          this.props.navigation.navigate('Info', {name: "lol"})
-                      }> Cliqué</Text>
+                <FlatList
+                    data={[{key: 'Slinding Puzzle'}, {key: 'TicTacToe'}, {key: 'GameOfLife'}, {key: 'Sudoku'}]}
+                    renderItem={({item}) =>
+                        <Text style={styles.button}
+                          onPress={() =>
+                              this.props.navigation.navigate('Info', {name: item.key})
+                          }>{item.key}</Text>
+                        }
+                />
+
             </View>
         );
     }
 }
-
+/*
+function getGames() {
+    return fetch('https://androidlessonsapi.herokuapp.com/api/game/list')
+}
+*/
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#d1ddff',
+        color: '#505463',
         alignItems: 'center',
         justifyContent: 'center',
     },
-    button1: {
-        backgroundColor: 'red'
+    text: {
+        flex: 1,
     },
-    button2: {
-        backgroundColor: 'green'
+    button: {
+        flex: 1,
+        backgroundColor: 'lightgrey'
     }
 });
